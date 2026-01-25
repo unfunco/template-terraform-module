f9f9238d32fa8cb135ddbbfb3c32bc8d5dec8967
Daniel Morris <daniel@honestempire.com>
2026-01-25 15:54:39 +0000

    chore: Add SPDX license headers to source files

--- variables.tf
+++ variables.tf
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2026 Daniel Morris <daniel@honestempire.com>
+// SPDX-License-Identifier: MIT
+
 variable "create" {
   default     = true
   description = "Enable/disable the creation of all resources."
